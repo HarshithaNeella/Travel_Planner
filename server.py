879e44c1bf7f019683ee7e2d0eaaaba6a6666d3f
HarshithaNeella <harshithaneella868@gmail.com>
2026-04-23 14:48:27 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+from pydantic import BaseModel
+
+load_dotenv()
+
+from mcp_server.tools.destination import get_destination_info
+from mcp_server.tools.itinerary import generate_plan
+from mcp_server.tools.images import get_image
+
+app = FastAPI()
+
+
+class ToolRequest(BaseModel):
+    tool: str
+    args: dict
+
+
+@app.post("/tool")
+def run_tool(req: ToolRequest):
+
+    if req.tool == "destination_info":
+        return get_destination_info(req.args.get("destination"))
+
+    elif req.tool == "generate_plan":
+        return generate_plan(
+            destination=req.args.get("destination"),
+            budget=req.args.get("budget"),
+            days=req.args.get("days")
+        )
+
+    elif req.tool == "get_image":
+        return get_image(req.args.get("destination"))
+
+    return {"error": "Unknown tool"}
